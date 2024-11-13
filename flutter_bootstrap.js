26d5d75310a33a3bd090e728640ee0f47c0f5bd1
Benard Mseti <andrewbenard7@gmail.com>
2024-11-13 17:48:17 +0300

    pull data from server

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2396337842"
+    serviceWorkerVersion: "1369224510"
   }
 });
